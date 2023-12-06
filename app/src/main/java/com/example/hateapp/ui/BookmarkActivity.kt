aad159f725582925c4e248422c168dd4571b1468
HateApp <hariramadhan37@gmail.com>
2023-12-06 15:29:04 +0700

    Progress Report

---
+++ app/src/main/java/com/example/hateapp/ui/BookmarkActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hateapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hateapp.R
+
+class BookmarkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmark)
+    }
+}
---
+++ app/src/main/java/com/example/hateapp/ui/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.hateapp.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hateapp.R
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.button3)
+        btnMoveActivity.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.button3 -> {
+                val moveIntent = Intent(this@MainActivity, ResultActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
+    }
+
+}
